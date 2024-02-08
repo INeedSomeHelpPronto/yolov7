b67997e74385ba97086a1908c83c790ebe844b3c
boolayon <bryanhahuy@gmail.com>
2024-02-08 16:28:57 -0500

    barcode scanning and printing

---
+++ barcode.py
@@ -0,0 +1,33 @@
+import requests, json
+import re
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def get_barcode(image):
+    """
+    Read the barcode from the picture and return the barcode number in EAN 13 format
+    """
+    barcode = info = None
+    print(image.shape)
+    img_raw = Image.fromarray(image)
+    img_raw.save("/content/WIN.jpg")
+    decoded = decode(img_raw)
+    
+    if decoded:
+        try:
+            barcode = int(re.findall("[+-]?[0-9][0-9]*|0$",
+                                    str(decoded[0].data))[0])
+            info = _get_product_info(barcode)
+        except:
+            pass
+
+    return barcode, info
+
+def _get_product_info(barcode):
+    """
+    Query a product by the barcode number the openFoodFact DB trought API 
+    """
+    address = "https://world.openfoodfacts.org/api/v0/product/{}.json".format(barcode)
+    #print(address) use for logging
+    r = requests.get(address)
+    return json.loads(r.text)
--- detect.py
+++ detect.py
@@ -2,6 +2,8 @@ import argparse
 import time
 from pathlib import Path
 
+from barcode import get_barcode
+
 import cv2
 import torch
 import torch.backends.cudnn as cudnn
@@ -103,6 +105,16 @@ def detect(save_img=False):
             else:
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
+            print(det)
+            center_x = int(det[0][0])
+            center_y = int(det[0][1])
+            w = int(det[0][2])
+            h = int(det[0][3])
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+            barcode, _ = get_barcode(im0[center_y: h, center_x:w])
+            print(barcode)
+            
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
