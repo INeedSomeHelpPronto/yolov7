f9939d3f3d3618b93be40d7fad49e6c465256a2e
boolayon <bryanhahuy@gmail.com>
2024-02-21 09:38:46 -0500

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -286,7 +286,11 @@ class LoadStreams:  # multiple IP or RTSP cameras
                 check_requirements(('pafy', 'youtube_dl'))
                 import pafy
                 url = pafy.new(url).getbest(preftype="mp4").url
-            cap = cv2.VideoCapture(url)
+            if s.startswith("nvarguscamerasrc"):
+                print("Running Gstreamer:")
+                cap = cv2.VideoCapture(url, cv2.CAP_GSTREAMER)
+            else:
+                cap = cv2.VideoCapture(url)
             assert cap.isOpened(), f'Failed to open {s}'
             w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
